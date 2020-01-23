7042a5d12a18658b8a11f45db94d2611c4cb6285
Coen Jacobs <coenjacobs@gmail.com>
2020-01-23 15:50:20 +0100

    Initial commit

---
+++ twig-loader.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Twig Loader
+ * Description: Loads the Twig template engine where it hasn't been included by the theme.
+ * Version: 1.0.0
+ * Author: Level Level
+ * Author URI: https://www.level-level.com
+ */
+
+require_once(dirname(__FILE__ ). '/vendor/autoload.php');
